b8b30e26493a709c757785b370f554062f8a9cea
Burak <buraksen7@hotmail.com>
2023-10-06 19:41:43 +0300

    Add color to Log implementation

--- src/Log/Log.cpp
+++ src/Log/Log.cpp
@@ -2,22 +2,22 @@
 
 void Log::info(std::string message)
 {
-    std::cout << "[INFO]: " << message << std::endl;
+    std::cout << "\033[1;32m[INFO]\033[0m: " << message << std::endl;
 }
 
 void Log::warn(std::string message)
 {
-    std::cout << "[WARN]: " << message << std::endl;
+    std::cout << "\033[1;33m[WARN]\033[0m: " << message << std::endl;
 }
 
 void Log::error(std::string message)
 {
-    std::cout << "[ERROR]: " << message << std::endl;
+    std::cout << "\033[1;31m[ERROR]\033[0m: " << message << std::endl;
 }
 
 void Log::fatal(std::string message)
 {
-    std::cout << "[FATAL]: " << message << std::endl;
+    std::cout << "\033[1;37m[FATAL]\033[0m: " << message << std::endl;
     exit(EXIT_FAILURE);
 }
 
